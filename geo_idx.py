8097f001bebe8abcd1e71e56d68cca86257b5668
Gaige Hunter Kerr <gaige.kerr@jhu.edu>
2018-09-11 15:24:32 -0400

    Return if value is far from intended value

--- geo_idx.py
+++ geo_idx.py
@@ -1,9 +1,8 @@
 def geo_idx(dd, dd_array):
-    import numpy as np
-    """
-    Function searches for nearest decimal degree in an array of decimal degrees and returns the 
-    index. np.argmin returns the indices of minimum value along an axis. So subtract dd from all 
-    values in dd_array, take absolute value and find index of minimum.
+    """Function searches for nearest decimal degree in an array of decimal 
+    degrees and returns the index. np.argmin returns the indices of minimum 
+    value along an axis. So subtract dd from all values in dd_array, take 
+    absolute value and find index of minimum.
     
     Parameters
     ----------
@@ -15,7 +14,15 @@ def geo_idx(dd, dd_array):
     Returns
     -------
     geo_idx : int
-        Index of latitude or longitude in dd_array that is closest in value to dd
+        Index of latitude or longitude in dd_array that is closest in value to 
+        dd
     """
+    import numpy as np    
     geo_idx = (np.abs(dd_array - dd)).argmin()
+    # if distance from closest cell to intended value is 2x the value of the
+    # spatial resolution, raise error 
+    res = np.diff(dd_array).mean()
+    if np.abs(dd_array[geo_idx] - dd) > (2 * res):
+        print('Closet index far from intended value!')
+        return 
     return geo_idx
